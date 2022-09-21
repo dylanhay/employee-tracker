18c992ff19b81c248000cdf9d181a24d3d6b582e
dchay <dylanhay@rogers.com>
2022-09-21 15:41:49 -0700

    server initialized

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ CREATE TABLE departments (
 CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INTEGER
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL
 );
 
 CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
+  role_id INTEGER NOT NULL,
   manager_id INTEGER,
 );
