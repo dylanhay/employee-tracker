3b6c0362d289e0681ce9bf984dc7354f87d404c3
dchay <dylanhay@rogers.com>
2022-09-22 11:31:20 -0700

    employee routes updated to join role data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,18 @@
-INSERT INTO employees (first_name, last_name)
+INSERT INTO roles (title, salary)
 VALUES
-  ('John', 'Lennon'),
-  ('Paul', 'McCartney'),
-  ('George', 'Harrison'),
-  ('Ringo', 'Starr'),
-  ('Brian', 'Epstein'),
-  ('George', 'Martin'),
-  ('Billy', 'Preston');
+  ('Guitarist', 100000),
+  ('Bassist', 100000),
+  ('Drummer', 90000),
+  ('Manager', 80000),
+  ('Producer', 80000),
+  ('Keyboardist', 80000);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+  ('John', 'Lennon', 1),
+  ('Paul', 'McCartney', 2),
+  ('George', 'Harrison', 1),
+  ('Ringo', 'Starr', 3),
+  ('Brian', 'Epstein', 4),
+  ('George', 'Martin', 5),
+  ('Billy', 'Preston', 6);
--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const db = mysql.createConnection(
 
 // Get all employees
 app.get("/api/employees", (req, res) => {
-  const sql = `SELECT * FROM employees`;
+  const sql = `SELECT employees.*, roles.title 
+             AS role_title 
+             FROM employees
+             LEFT JOIN roles
+             ON employees.role_id = roles.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -38,7 +42,12 @@ app.get("/api/employees", (req, res) => {
 
 // Get a single employee
 app.get("/api/employee/:id", (req, res) => {
-  const sql = `SELECT * FROM employees WHERE id = ?`;
+  const sql = `SELECT employees.*, roles.title 
+             AS role_title 
+             FROM employees
+             LEFT JOIN roles
+             ON employees.role_id = roles.id
+             WHERE employees.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
